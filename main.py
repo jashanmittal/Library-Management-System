913da9b58fa81ac278ab49173805e285b7cb80f2
JashanMittal <jashanmittal9876@gmail.com>
2026-06-28 00:25:30 +0530

    Fixed bugs

--- main.py
+++ main.py
@@ -14,9 +14,20 @@ class Book():
         for index, book in enumerate(books, start=1):
             print(f"""
 ------------------------
-{index}. Name : {self.name}
-Author : {self.author}
+{index}. Name : {book.name}
+Author : {book.author}
 ------------------------""")
-        user = input("Which Book Would You Like To Get On Rent?\n")
-        print(f"You rented the {books(user - 1)}")
+        user = int(input("Which Book Would You Like To Get On Rent?\n"))
+        chosen_book = books[user - 1]
+        print(f"You rented {chosen_book['Name']}")
+
+    def add_book(self):
+        name = input("Name : ")
+        author = input("Author : ")
+        price = float(input("Price : "))
+        new_book = Book(name, author, price)
+        books.append(new_book)
+        print("Book Successfully Added")
+        time.sleep(1)
+
             
