0ba31b6921970f4e38e43b38a5dbf09a6123b32f
JashanMittal <jashanmittal9876@gmail.com>
2026-06-28 22:08:53 +0530

    Completed Book Class and fixed bugs and started User class

--- main.py
+++ main.py
@@ -30,4 +30,39 @@ Author : {book.author}
         print("Book Successfully Added")
         time.sleep(1)
 
-            
+    def delete_book(self):
+        for index, book in enumerate(books, start=1):
+            print(f"""
+------------------------
+{index}. Name : {book.name}
+Author : {book.author}
+Price : {book.price}
+------------------------""")
+        user = int(input("Which Book Would You Delete?\n"))
+        books.pop(user - 1)
+        print("Successfully Deleted Book From Database")
+        time.sleep(1)
+
+    def buy_book(self):
+        for index, book in enumerate(books, start=1):
+            print(f"""
+------------------------
+{index}. Name : {book.name}
+Author : {book.author}
+------------------------""")
+        user = int(input("Which Book Would You Like To Buy?\n"))
+        chosen_book = books[user - 1]
+        print(f"You Bought {chosen_book['Name']}")
+
+
+class User():
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+        self.history = []
+
+    def register(self):
+        pass
+
+    def login(self):
+        pass
